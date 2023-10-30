42578b1032e7438a17a7e4f149c7c5c3e237d156
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-10-30 17:06:09 +0300

    Front-end 1

---
+++ components/CartegoryScroll.jsx
@@ -0,0 +1,57 @@
+import * as React from "react"
+import Image from "next/image"
+
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import Link from "next/link"
+
+
+export const Cartegories = [
+    {
+        name: "Vans",
+        image: "https://images.unsplash.com/photo-1563183222-ff776d1076e1?auto=format&fit=crop&q=80&w=300&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Boots",
+        image: "https://images.unsplash.com/photo-1605733160314-4fc7dac4bb16?auto=format&fit=crop&q=80&w=300&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Heels",
+        image: "https://images.unsplash.com/photo-1596702874230-b5706dfb5bc7?auto=format&fit=crop&q=80&w=300&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Sneakers",
+        image: "https://images.unsplash.com/photo-1549298916-b41d501d3772?auto=format&fit=crop&q=80&w=300&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+]
+
+export function ScrollAreaHorizontalDemo() {
+    return (
+        <ScrollArea className="justify-center items-center align-middle mx-auto whitespace-nowrap ">
+            <div className="flex w-max space-x-4 p-4">
+                {Cartegories.map((Cartegory) => (
+                    <Link className="hover:opacity-75" key={Cartegory.name} href={`/Shop/${Cartegory.name}`} >
+
+                        <figure key={Cartegory.name} className=" flex-1 shrink-0">
+                            <div className="overflow-hidden rounded-md">
+                                <Image
+                                    src={Cartegory.image}
+                                    alt={Cartegory.name}
+                                    className="aspect-[3/4] h-fit w-fit object-cover"
+                                    width={300}
+                                    height={400}
+                                />
+                            </div>
+                            <figcaption className="pt-3 text-xl text-muted-foreground">
+                                <span className="font-bold text-accent-foreground ">
+                                    {Cartegory.name}
+                                </span>
+                            </figcaption>
+                        </figure>
+
+                    </Link>
+                ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+    )
+}
---
+++ components/NavBar.jsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Link from "next/link"
+import { ModeToggle } from "./ModeToggle"
+
+const NavBar = () => {
+    const Navlinks = [
+        {
+            tag: "Home",
+            path: "/"
+        },
+        {
+            tag: "Shop",
+            path: "/Shop"
+        },
+        {
+            tag: "Contact",
+            path: "/Contact"
+        }
+    ]
+    return (
+        <nav className=" z-40 sticky top-0 mx-auto flex flex-row justify-between w-full px-4 py-2 items-center bg-transparent rounded-b-lg text-xl font-semibold align-middle">
+            <section className="flex gap-4 h-full align-middle items-center">
+                <Link href='/' className="text-4xl font-extrabold p-1 text-blue-500 mr-6 hover:text-sky-600">BZ</Link>
+                {Navlinks.map((link) => (
+                    <Link
+                        key={link.tag}
+                        className="text-blue-400 hover:text-sky-200 hover:underline active:underline"
+                        href={`${link.path}`}>
+                        {link.tag}
+                    </Link>))
+                }
+            </section>
+            <ModeToggle />
+        </nav>
+    )
+}
+
+export default NavBar
