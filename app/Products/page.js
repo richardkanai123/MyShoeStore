3378c32c524ba7c8dd9c7b10bb370f0b7d7d99b2
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-08 13:39:04 +0300

    throw new Error() removed 2

--- app/Products/page.js
+++ app/Products/page.js
@@ -6,7 +6,6 @@ const getShoes = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/shoes`)
     if (!res.ok) {
         console.log('Error occured')
-        throw new Error('Failed to fetch data')
     }
     const data = await res.json()
     return data.Shoes
