57e065f6f06fe4575691507d2bf7fbbc960d6936
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-08 12:22:54 +0300

    Product page getShoes function redone

--- app/Products/page.js
+++ app/Products/page.js
@@ -2,14 +2,19 @@ import ProductCard from '@/components/ProductCard'
 import { Suspense } from 'react'
 
 
-const Page = async () => {
+const getShoes = async () => {
     const res = await fetch(`${process.env.BASE_URL}/api/shoes`)
     if (!res.ok) {
-        // throw new Error("Fetching products failed")
         console.log('Error occured')
+        throw new Error('Failed to fetch data')
     }
     const data = await res.json()
+    return data.Shoes
+}
 
+const Page = async () => {
+
+    const shoesList = await getShoes()
 
     return (
         <>
@@ -18,7 +23,7 @@ const Page = async () => {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-4 pb-2">
                 <Suspense fallback={<p>Loading Shoes</p>}>
                     {
-                        data.Shoes.map((shoe) => (
+                        shoesList.map((shoe) => (
                             <ProductCard Shoe={shoe} key={shoe.shoeId} />
                         ))
                     }
