7eb61665c559306329f4b8d324599e5185f5383a
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-08 15:49:12 +0300

    URL check before fecth 2

--- app/shop/page.js
+++ app/shop/page.js
@@ -2,10 +2,7 @@ import ShoeList from "@/components/ShoeList"
 
 
 async function getShoesData() {
-    if (`${process.env.NEXT_PUBLIC_BASEURL}` === undefined) {
-        throw new Error('Failed to fetch data')
-        return null;
-    }
+
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/shoes`, {
         cache: 'no-cache',
     }
@@ -20,6 +17,9 @@ async function getShoesData() {
 }
 
 const Page = async () => {
+    if (`${process.env.NEXT_PUBLIC_BASEURL}` === undefined) {
+        return null;
+    }
     const data = await getShoesData()
     return (
         <div className="p-2 flex flex-col gap-2 ">
