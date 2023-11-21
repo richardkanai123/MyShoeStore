8195f141b52e75883a097f6949dd5c866cd30f5c
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-21 15:24:26 +0300

    Test without Home Link

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -13,10 +13,10 @@ const NavBar = () => {
     const pathName = usePathname()
 
     const Navlinks = [
-        {
-            tag: "Home",
-            path: "/"
-        },
+        // {
+        //     tag: "Home",
+        //     path: "/"
+        // },
         {
             tag: "Shop",
             path: "/shop"
@@ -32,7 +32,7 @@ const NavBar = () => {
     return (
         <nav className=" z-40 sticky top-0 mx-auto flex flex-row justify-around w-full px-4 py-2 items-center bg-transparent rounded-b-lg text-base md:text-xl font-semibold align-middle border-b snap-proximity shadow-md">
             <section className="flex gap-4 h-full align-middle items-center justify-around ">
-                <Link href='/' className="text-2xl md:text-4xl font-semibold md:font-extrabold p-1 text-blue-500 mr-6 hover:text-sky-600">BZ</Link>
+                <Link href='/' className="text-2xl md:text-4xl font-semibold md:font-extrabold p-1 text-blue-500 mr-4 hover:text-sky-600">BZ</Link>
                 {Navlinks.map((link) => (
                     <Link
                         key={link.tag}
